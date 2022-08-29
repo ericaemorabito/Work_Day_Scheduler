327797c569e1ba200511d7aa0950e3f05df1a6c3
EricaE <106550558+ericaemorabito@users.noreply.github.com>
2022-08-29 09:48:36 -0500

    Day displayed

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var currentDayEl = $('#currentDay');
-currentDay = moment().format('dddd'); 
-console.log(currentDay)
 
-//TODO: not working
 function displayDay() {
   var currentDay = moment().format('dddd'); 
-  console.log(currentDay); // logs current day
   currentDayEl.text(currentDay);
 }
+displayDay();
 
-//TODO: create 8 rows for 8 business hours and 3 columns for hour, space, save button
-function print
+//TODO: create td for each next row, append each, set data to x 
+function createTimeBlocks(){
+
+};
+createTimeBlocks();
