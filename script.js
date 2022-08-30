e32271517b2d651116eaec4224f2915e021291fb
EricaE <106550558+ericaemorabito@users.noreply.github.com>
2022-08-29 21:50:46 -0500

    Notes added about local storage

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 function createTimeBlocks() {
   for (let i=0; i < hours.length; i++){
 //create new row
-var newRow = $('<tr>')
+var newRow = $('<tr>').addClass('.time-block', 'row');
 //append new row to table
 timeTable.append(newRow);
 //create td, set data td to index of hour, append to new row
@@ -24,7 +24,7 @@ timeTable.append(newHour);
 var newSchedule = $('<td>').text('Schedule area').addClass('schedule').attr('contenteditable', 'true');
 timeTable.append(newSchedule);
 //create td, set to <a> with save, append to new row
-var saveLink = $('<a>').text('Save').attr('href', '#').addClass('save');
+var saveLink = $('<a>').text('Save').attr('href', '#').addClass('saveBtn');
 timeTable.append(saveLink);
 
 //TODO: set time blocks to color depending on their time
@@ -32,11 +32,11 @@ var currentTime = moment().format('HH');
 
 var timeColoring = function(){
   if (currentTime < hours[i]){
-    newSchedule.addClass('past');
+    newSchedule.addClass('future');
   } else if (currentTime === hours[i]) {
     newSchedule.addClass('present');
   } else {
-    newSchedule.addClass('future');
+    newSchedule.addClass('past');
   }
 };
 timeColoring();
@@ -44,10 +44,14 @@ timeColoring();
 };
 createTimeBlocks();
 
+//TODO: put id's on the <td> for schedule
 //TODO: when click save, save schedule input to local storage
-var scheduleInput = $('#schedule').val(); //gets value of schedule <td>
+var scheduleInput = $('#schedule').val(); //TODO: 
 console.log(scheduleInput);
 
 $('.save').on('click', function() { //when save link is clicked, set value of schedule to local input
+  //TODO: get input in schedule block
+  //TODO: set to storage array
+  //TODO: create temp array local storage
     localStorage.setItem('schedule', scheduleInput)
   });
