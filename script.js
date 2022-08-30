f9f43d965533771271622a039f06455a33962327
EricaE <106550558+ericaemorabito@users.noreply.github.com>
2022-08-30 11:56:30 -0500

    Padding added to Time Blocks

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ displayDay();
 
 var timeBlocks = $('#time_blocks');
 var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var storedSchedule = ['schedule 9', 'schedule 10', 'schedule 11', 'schedule 12', 'schedule 13', 'schedule 14', 'schedule 15', 'schedule 16', 'schedule 17'];
 
 //TODO List:
 //add event listener to each button within loop
@@ -18,17 +19,15 @@ var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 function createTimeBlocks() {
   for (let i = 0; i < hours.length; i++) {
     let newRow = $('<section>').addClass('time-block col-12 d-inline-flex justify-content-center');
-    timeBlocks.append(newRow);
-
-    let newHour = $('<li>').text(hours[i]).addClass('hour col-2 p-3');
-    newRow.append(newHour);
-
-    let newSchedule = $('<li>').text('Schedule area').attr('contenteditable', 'true').addClass('col-2 d-flex p-3');
-    newRow.append(newSchedule);
+    let newHour = $('<li>').text(hours[i]).addClass('hour col-2');
+    let newSchedule = $('<li>').text('Schedule area').attr('contenteditable', 'true').addClass('col-12 d-flex');
     newSchedule.attr('data-hour', hours[i]);
+    let saveBtn = $('<button>').text('Save').addClass('d-flex col-2 saveBtn');
 
-    let saveLink = $('<button>').text('Save').addClass('d-flex col-2 saveBtn p-3');
-    newRow.append(saveLink);
+    timeBlocks.append(newRow);
+    newRow.append(newHour);
+    newRow.append(newSchedule);
+    newRow.append(saveBtn);
 
     let currentTime = moment().format('HH');
     let timeColoring = function () {
@@ -41,15 +40,22 @@ function createTimeBlocks() {
       }
     };
     timeColoring();
+
+    //TODO LOCAL STORAGE 
+
+    let scheduleInput = $('#schedule').text();
+    saveBtn.on('click', function () {
+      //save to local storage
+      localStorage.setItem('schedule', scheduleInput);
+    })
   }
 };
 createTimeBlocks();
 
 //TODO: Local Storage
-
 // Get data typed into schedule section
-var scheduleInput = $('#schedule').text();
-console.log(scheduleInput);
+// var scheduleInput = $('#schedule').text();
+// console.log(scheduleInput);
 
 //Get Stored Items
 var getLocalStorage = function () {
@@ -65,7 +71,7 @@ $('.saveBtn').on('click', function () {
 });
 
 //TODO: 
-var init = function () {
-  getLocalStorage();
-  $('schedule').text()
-};
+// var init = function () {
+//   getLocalStorage();
+//   $('schedule').text()
+// };
