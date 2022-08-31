250f1a569f583436fd9b28d33d0cf62e1f522f98
EricaE <106550558+ericaemorabito@users.noreply.github.com>
2022-08-31 09:09:08 -0500

    Save icon added, center formatted

--- script.js
+++ script.js
@@ -14,39 +14,39 @@ localStorage.setItem('data', storedData);
 var storedData = [
   {
     hour: 9,
-    schedule: 'sch9'
+    schedule: ''
   },
   {
     hour: 10,
-    schedule: 'sch10'
+    schedule: ''
   },
   {
     hour: 11,
-    schedule: 'sch11'
+    schedule: ''
   },
   {
     hour: 12,
-    schedule: 'sch12'
+    schedule: ''
   },
   {
     hour: 13,
-    schedule: 'sch13'
+    schedule: ''
   },
   {
     hour: 14,
-    schedule: 'sch14'
+    schedule: ''
   },
   {
     hour: 15,
-    schedule: 'sch15'
+    schedule: ''
   },
   {
     hour: 16,
-    schedule: 'sch16'
+    schedule: ''
   },
   {
     hour: 17,
-    schedule: 'sch17'
+    schedule: ''
   }
 ];
 
@@ -56,8 +56,9 @@ function createTimeBlocks() {
     let newHour = $('<li>').text(storedData[i].hour).addClass('hour col-1');
     let newSchedule = $('<li>').text(storedData[i].schedule).attr('contenteditable', 'true').addClass('schedule col-9 d-flex');
     newSchedule.attr('data-hour', storedData[i].hour); //Sets the data-hour of each schedule to the hour example: data-hour="9"
-    let saveBtn = $('<button>').text('Save').addClass('d-flex col-1 saveBtn');
-
+    let saveBtn = $('<button>').addClass('d-flex col-1 saveBtn');
+    let saveIcon = $('<i>').addClass('fas fa-save');
+    saveBtn.append(saveIcon);
     timeBlocks.append(newRow);
     newRow.append(newHour);
     newRow.append(newSchedule);
@@ -77,9 +78,8 @@ function createTimeBlocks() {
     timeColoring();
 
     //TODO: LOCAL STORAGE 
-
     saveBtn.on('click', function () {
-      let scheduleEl = saveBtn.siblings('.schedule'); //Finds the schedule element next to save button clicked
+      scheduleEl = saveBtn.siblings('.schedule'); //Finds the schedule element next to save button clicked
       let newScheduleInput = scheduleEl.text(); //Gets the text in the schedule element
       console.log(newScheduleInput);
       localStorage.setItem('schedule', newScheduleInput); //Set to local storage
@@ -87,13 +87,15 @@ function createTimeBlocks() {
     })
     //TODO: render item in local storage when page loads
     // var init = function () {
-    //   var scheduleInit = localStorage.getItem('schedule'); // Data retrieved from storage
-    //   scheduleEl.text(scheduleInit);//set text to schedule element
+    //   var scheduleSaved = localStorage.getItem('schedule'); // Data retrieved from storage
+    //   scheduleEl.text(scheduleSaved);//set schedule element's text to saved input
     // };
     // init();
   }
 };
 createTimeBlocks();
+
+var scheduleEl;
 
 //1. find sibling schedule element .sibling('.schedule')
 //2. get the new input data
